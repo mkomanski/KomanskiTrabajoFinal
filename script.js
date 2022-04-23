ede051b65113c9a9147fd85b3634ec78601528ba
Marcela <marcelakomanski@gmail.com>
2022-04-23 18:11:21 -0300

    html, css y video listo

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 /* MARCELA SUSANA KOMANSKI - DNI 23771567 */
 
-/* falta animar logo del menu del nav */
+
+/* Animación del logo del menú*/
+
+
+
+/* Animación de botones al clickear*/
+
+
+
+
 
 
 /* Video */
 
+let botonReproducir = document.getElementById("play");
+let botonPausar = document.getElementById("pause");
+let video = document.getElementById("video_historia");
 
+botonReproducir.addEventListener("click",()=>{
+    video.play();
+})
+
+botonPausar.addEventListener("click",()=>{
+    video.pause();
+})
 
 /*Drag And Drop */
+
+
 
