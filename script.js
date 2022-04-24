bd5e24508f90219341fd315895f249b2ce14895f
Marcela <marcelakomanski@gmail.com>
2022-04-24 11:26:53 -0300

    falta animar logo y drag&drop-trabajando en ellos

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 /* MARCELA SUSANA KOMANSKI - DNI 23771567 */
 
 
-/* Animación del logo del menú*/
+
+
+/* Animación del logo del menú -- no me sale.
+
+let monitorGr1 = document.getElementById ("monitorGr1");
+let monitorGr2 = document.getElementById ("monitorGr2");
+let tecladoGr1 = document.getElementById ("tecladoGr1");
+let tecladoGr2 = document.getElementById ("tecladoGr2");
+
+monitorGr1.addEventListener("onload",()=>{
+    monitorGr1.style.translate (0,0);
+})
+
+*/
 
 
 
-/* Animación de botones al clickear*/
 
-
-
-
+/*animación de botones: hecho con css :active */
 
 
 /* Video */
